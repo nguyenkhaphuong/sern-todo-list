5ecb1ebb3d2f41c17bbf3d19cd661a9f42841660
Nguyễn Khả Phương <phuongkha1506@gmail.com>
2023-09-11 14:25:14 +0700

    Add Redux Store

---
+++ client/src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { authReducer, tasksReducer, userReducer } from './slices/slices'
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    user: userReducer,
+    task: tasksReducer,
+  },
+})
+
+export default store
