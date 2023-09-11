aa0fd886842385f9bfe26983de2c3e7a82c0b167
Nguyễn Khả Phương <phuongkha1506@gmail.com>
2023-09-11 14:24:38 +0700

    Add CORS origin client URL

--- server/server.js
+++ server/server.js
@@ -11,8 +11,13 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors())
+const corsOptions = {
+  origin: process.env.CLIENT_HOST,
+  methods: ['GET', 'POST'],
+}
+
 app.use(bodyParser.json())
+app.use(cors(corsOptions))
 
 // Connect to MySQL database
 const dbConfig = {
@@ -25,6 +30,11 @@ const db = mysql2.createConnection(dbConfig)
 db.connect((err) => {
   if (err) throw err
   console.log('Connected to database')
+})
+
+//Launch the server
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
 })
 
 //User Routes for authentication
@@ -42,6 +52,10 @@ const authenticateToken = (req, res, next) => {
       return res.status(403).send('Invalid token')
     }
 
+    if (!req.user_id) {
+      return res.redirect('/login')
+    }
+
     req.user_id = decoded.id
     next()
   })
@@ -49,24 +63,26 @@ const authenticateToken = (req, res, next) => {
 
 //Retrieve the tasks from the database
 app.get('/', authenticateToken, (req, res) => {
+  const userId = req.user
   const sql =
     'SELECT * FROM tasks WHERE user_id = ? ORDER BY FIELD(priority,"high","medium","normal")'
 
-  db.query(sql, (err, results) => {
+  db.query(sql, [userId], (err, results) => {
     if (err) {
-      throw err
+      return res.status(500).json({ error: 'Failed to retrieve tasks' })
     }
-    res.send(results)
+    res.status(200).send(results)
   })
 })
 
 //Add new task
 app.post('/', authenticateToken, (req, res) => {
   const { title, description, priority } = req.body
+  const userID = req.user_id
   const sql =
     'INSERT INTO tasks (title, description, priority, status, user_id) VALUES (?, ?, ?, "Todo", ?)'
 
-  db.query(sql, [title, description || null, priority], (err) => {
+  db.query(sql, [title, description || null, priority, userID], (err) => {
     if (err) {
       throw err
     }
@@ -75,7 +91,7 @@ app.post('/', authenticateToken, (req, res) => {
 })
 
 //Update the task
-app.put('/:id', (req, res) => {
+app.put('/:id', authenticateToken, (req, res) => {
   const { id } = req.params
   const { title, priority, description, status } = req.body
   const sql =
@@ -90,7 +106,7 @@ app.put('/:id', (req, res) => {
 })
 
 //Delete the task
-app.delete('/:id', (req, res) => {
+app.delete('/:id', authenticateToken, (req, res) => {
   const { id } = req.params
   const sql = 'DELETE FROM tasks WHERE id = ? AND user_id = ?'
 
@@ -100,9 +116,4 @@ app.delete('/:id', (req, res) => {
     }
     res.send('Task deleted successfully')
   })
-})
-
-//Launch the server
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
 })
