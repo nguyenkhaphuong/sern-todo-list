8a289736e5f9b568cf18da1169db7fc24e5578a9
Nguyễn Khả Phương <phuongkha1506@gmail.com>
2023-09-11 14:25:31 +0700

    Add Redux Slices

---
+++ client/src/redux/slices/taskSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = []
+
+const taskSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTask: (state, action) => {
+      state.push(action.payload)
+    },
+    updateTask: (state, action) => {
+      const { id, title, description, priority, status } = action.payload
+      const existingTask = state.find((task) => task.id === id)
+      if (existingTask) {
+        existingTask.title = title
+        existingTask.description = description
+        existingTask.priority = priority
+        existingTask.status = status
+      }
+    },
+    deleteTask: (state, action) => {
+      const id = action.payload
+      const index = state.findIndex((task) => task.id === id)
+      if (index !== -1) {
+        state.splice(index, 1)
+      }
+    },
+  },
+})
+
+export const { addTask, deleteTask, updateTask } = taskSlice.actions
+
+export default taskSlice.reducer
