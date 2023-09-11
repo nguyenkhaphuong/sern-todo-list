0fe41ec5f0e54556c2a104296d1247a626a689b2
Nguyễn Khả Phương <phuongkha1506@gmail.com>
2023-09-11 14:27:00 +0700

    Add Redux Store Provider

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import store from './redux/store'
+import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 )
